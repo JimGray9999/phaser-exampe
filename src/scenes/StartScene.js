e70276b9bb34d2d0619b6371ec11820b292a755b
Jim Gray <jimgray2005@gmail.com>
2020-07-17 00:00:32 -0400

    added text message to start scene

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -9,6 +9,10 @@ export default class StartScene extends Phaser.Scene
 
     preload()
     {
+        const startText = this.add.text(100,100, 'Phaser Game Example!');
+
+        const pressButtonInstruction = this.add.text(200,200, 'Click mouse to begin')
+
         this.input.on('pointerup', function() {
             this.scene.start('game-scene');
         }, this);
